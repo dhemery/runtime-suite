93f5d34c826393d186af3388f3f8f28c48a1c79f
Dale Emery <dale@dhemery.com>
2010-10-17 04:17:47 -0700

    Make core a module

--- runtime-suite-core/src/test/java/com/dhemery/runtimesuite/finders/AClassesOnClasspathFinder.java
+++ runtime-suite-core/src/test/java/com/dhemery/runtimesuite/finders/AClassesOnClasspathFinder.java
@@ -12,7 +12,7 @@ import static org.fest.assertions.Assertions.*;
 
 public class AClassesOnClasspathFinder {
 	@Test public void findsAllClassesOnASingleElementClasspath() {
-		String classpath = "./classpath1/target/test-classes";
+		String classpath = "../classpath1/target/test-classes";
 		ClassFinder finder = new ClassesOnClassPath(classpath);
 		Collection<Class<?>> found = finder.find();
 		assertThat(found).containsOnly(a.Test_a_1.class,
@@ -28,9 +28,9 @@ public class AClassesOnClasspathFinder {
 	}
 
 	@Test public void findsAllClassesOnAMultipleElementClasspath() {
-		String classpath = "./classpath1/target/test-classes"
+		String classpath = "../classpath1/target/test-classes"
 							+ File.pathSeparator
-							+ "./classpath2/target/test-classes";
+							+ "../classpath2/target/test-classes";
 		ClassFinder finder = new ClassesOnClassPath(classpath);
 		Collection<Class<?>> found = finder.find();
 		assertThat(found).containsOnly(a.Test_a_1.class,
@@ -59,7 +59,7 @@ public class AClassesOnClasspathFinder {
 		// classpath.three contains
 		//    - ./c/Test_c_1.class
 		//    - ./c/not-a-test.txt
-		String classpath = "./classpath3/target/test-classes";
+		String classpath = "../classpath3/target/test-classes";
 		ClassFinder finder = new ClassesOnClassPath(classpath);
 		Collection<Class<?>> found = finder.find();
 		assertThat(found).containsOnly(c.Test_c_1.class);
@@ -69,7 +69,7 @@ public class AClassesOnClasspathFinder {
 		// classpath.four contains
 		//    - ./d/Test_d_1.class
 		//    - ./d/NotATest_d_2.class
-		String classpath = "./classpath4/target/test-classes";
+		String classpath = "../classpath4/target/test-classes";
 		ClassFinder finder = new ClassesOnClassPath(classpath);
 		Collection<Class<?>> found = finder.find();
 		assertThat(found).containsOnly(d.Test_d_1.class);
