f1530259430c9e600ae60b61c5655e3ef671bb61
Dale Emery <dale@dhemery.com>
2010-10-19 01:31:50 -0700

    Add a note about the horribly cheesy workaround in RunnableClassRunner

--- src/main/java/com/dhemery/runtimesuite/internal/RunnableClassRunner.java
+++ src/main/java/com/dhemery/runtimesuite/internal/RunnableClassRunner.java
@@ -11,6 +11,11 @@ public class RunnableClassRunner extends BlockJUnit4ClassRunner {
 	private static List<FrameworkMethod> runnableMethods;
 
 	public RunnableClassRunner(RunnableClass runnable) throws InitializationError {
+		// This calls the static method testClass() to initialize runnableMethods.
+		// This is a horrible idea. But computeTestMethods() is called during
+		// super(), and it needs runnableMethods. So I have to initialize
+		// runnableMethods before calling super(). Gack.
+		// TODO: Find a better way.
 		super(testClass(runnable));
 	}
 
