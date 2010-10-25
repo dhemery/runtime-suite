96b6aea77f7c3d95daf436fa9f73b0b8bed0b667
Dale Emery <dale@dhemery.com>
2010-10-25 12:57:34 -0700

    Remove old ListedClassFinder

--- core/src/test/java/examples/ListedClassFinder.java
+++
@@ -1,9 +0,0 @@
-package examples;
-
-import com.dhemery.runtimesuite.ClassFinder;
-
-public class ListedClassFinder extends ListedClassNonFinder implements ClassFinder {
-	public ListedClassFinder(Class<?>...classes) {
-		super(classes);
-	}
-}
